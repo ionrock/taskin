91d104a25db499ccef54878dcbfce42dbb4aa932
Eric Larson <eric@ionrock.org>
2016-02-10 10:56:07 +0000

    Add totally untested pools ;)

--- taskin/task.py
+++ taskin/task.py
@@ -1,4 +1,6 @@
-import abc
+from multiprocessing import Pool as ProcessPool
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import cpu_count
 
 
 def do_flow(flow, result=None):
@@ -8,19 +10,41 @@ def do_flow(flow, result=None):
     return result
 
 
+class PoolAPI(object):
+    def map(self, *args, **kw):
+        return self.pool.map(*args, **kw)
+
+
+class ThreadPool(PoolAPI):
+
+    def __init__(self, size=20):
+        self.size = size
+        self.pool = ThreadPool(self.size)
+
+
+class ProcessPool(PoolAPI):
+
+    def __init__(self, size=None):
+        self.size = size or cpu_count()
+        self.pool = ProcessPool(self.size)
+
+
 class MapTask(object):
 
-    def __init__(self, args, task):
+    pool_types = [
+        'thread', 'process'
+    ]
+
+    def __init__(self, args, task, pool):
         self.args = args
         self.task = task
-        self.pool = Pool(cpu_count())
+        self.pool = pool
 
     def iter_input(self, input):
         for args in self.args:
             if not isinstance(args, (tuple, list)):
                 args = [args]
             yield tuple([input] + args)
-
 
     def __call__(self, input):
         return self.pool.map(self.task, self.iter_input(input))
